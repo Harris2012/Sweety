bc9846718c45a2de194a203b8072d3d013e4d512
HarrisZhang <hc_zhang@Ctrip.com>
2017-11-30 10:45:48 +0800

    update

---
+++ Sweety/Entity/BusinessEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sweety.Entity
+{
+    [ExcelTable("交易表")]
+    public class BusinessEntity
+    {
+        [ExcelColumn("交易号", 1)]
+        public string BusinessNo { get; set; }
+
+        [ExcelColumn("名称", 2)]
+        public string BusinessName { get; set; }
+    }
+}
---
+++ Sweety/Entity/SourceEnity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sweety.Entity
+{
+    [ExcelTable("基础数据表")]
+    public class SourceEnity
+    {
+        [ExcelColumn("单据号", 1)]
+        public string PaperNo { get; set; }
+
+        [ExcelColumn("交易号", 2)]
+        public string BusinessNo { get; set; }
+
+        [ExcelColumn("商品编号", 3)]
+        public string ProductNo { get; set; }
+
+        /// <summary>
+        /// 采购 或 销售
+        /// </summary>
+        [ExcelColumn("类型", 4)]
+        public string Mode { get; set; }
+
+        [ExcelColumn("数量", 5)]
+        public int Count { get; set; }
+
+        [ExcelColumn("日期", 6)]
+        public DateTime CreateTime { get; set; }
+    }
+}
